7750210c084f72deed40994b9e5f98776bfc6947
iwonakulacz <iwonakulacz@gmail.com>
2019-08-19 17:33:32 +0200

    add gh pages

---
+++ build/precache-manifest.6ab4bb0c30f26807e2237d3babec044f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f438da276631c529aee9",
+    "url": "/myNotesApp/static/js/runtime~main.f438da27.js"
+  },
+  {
+    "revision": "37b5a9a750b763dadc54",
+    "url": "/myNotesApp/static/js/main.37b5a9a7.chunk.js"
+  },
+  {
+    "revision": "4b5a48fd6e521e60c040",
+    "url": "/myNotesApp/static/js/1.4b5a48fd.chunk.js"
+  },
+  {
+    "revision": "37b5a9a750b763dadc54",
+    "url": "/myNotesApp/static/css/main.35180bd4.chunk.css"
+  },
+  {
+    "revision": "3e98d68e7c7fda810c51fe58eee7b522",
+    "url": "/myNotesApp/index.html"
+  }
+];
